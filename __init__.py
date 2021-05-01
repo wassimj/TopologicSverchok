afd11c4c622773f179c76154f2902d56e9b08215
Wassim Jabi <wassim.jabi@gmail.com>
2021-05-01 18:50:58 +0100

    Updated version number to 0.5.3

--- __init__.py
+++ __init__.py
@@ -17,7 +17,7 @@
 bl_info = {
     "name": "Topologic",
     "author": "Wassim Jabi",
-    "version": (0, 5, 2, 0),
+    "version": (0, 5, 3, 0),
     "blender": (2, 92, 0),
     "location": "Node Editor",
     "category": "Node",
