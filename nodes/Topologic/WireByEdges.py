a3f7e165803c3f0dded074f9e66c0250a2685a46
Wassim Jabi <wassim.jabi@gmail.com>
2021-09-01 09:33:02 +0300

    Updated Wire.ByEdges
    Wire.ByEdges now does not require a head to tail edges. It can create any wire (open or closed) from a set of intersecting edges. The only requirement is that the edges (in total) do intersect to create one connected wire. If the edges are not intersecting, you should use Cluster.ByTopologies instead.

--- nodes/Topologic/WireByEdges.py
+++ nodes/Topologic/WireByEdges.py
@@ -4,6 +4,7 @@ from sverchok.node_tree import SverchCustomTreeNode
 from sverchok.data_structure import updateNode
 
 import topologic
+from topologic import Vertex, Edge, Wire, Face, Shell, Cell, CellComplex, Cluster, Topology, Dictionary
 import cppyy
 
 # From https://stackabuse.com/python-how-to-flatten-list-of-lists/
@@ -16,12 +17,40 @@ def flatten(element):
 		returnList = [element]
 	return returnList
 
+def classByType(argument):
+	switcher = {
+		1: Vertex,
+		2: Edge,
+		4: Wire,
+		8: Face,
+		16: Shell,
+		32: Cell,
+		64: CellComplex,
+		128: Cluster }
+	return switcher.get(argument, Topology)
+
+def fixTopologyClass(topology):
+  topology.__class__ = classByType(topology.GetType())
+  return topology
+
 def processItem(item):
 	wire = None
 	edges = cppyy.gbl.std.list[topologic.Edge.Ptr]()
 	for anEdge in item:
-		edges.push_back(anEdge)
-	wire = topologic.Wire.ByEdges(edges)
+		if anEdge.GetType() == 2:
+			if wire == None:
+				wire = anEdge
+			else:
+				try:
+					wire = wire.Merge(anEdge)
+				except:
+					continue
+		# edges.push_back(anEdge)
+	# wire = topologic.Wire.ByEdges(edges)
+	if wire.GetType() != 4:
+		raise Exception("Error: Could not create Wire. Please check input")
+	else:
+		wire = fixTopologyClass(wire)
 	return wire
 
 def recur(input):
