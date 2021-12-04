c8f1c5ab1dff0197ce1eecf3f7f3c020fdcb3a56
Wassim Jabi <wassim.jabi@gmail.com>
2021-12-04 08:44:23 +0000

    Updated version and removed errant file

--- nodes/Topologic/Version.py
+++ nodes/Topologic/Version.py
@@ -18,7 +18,7 @@ class SvTopologicVersion(bpy.types.Node, SverchCustomTreeNode):
 		self.outputs.new('SvStringsSocket', 'Version')
 
 	def process(self):
-		self.outputs['Version'].sv_set(['0 6 0 5'])
+		self.outputs['Version'].sv_set(['0 6 0 6'])
 
 
 def register():
